698391e5648fffbd2bd6397d45a67bf8d2b3ef60
Anthony Moore <antleemoore@hotmail.com>
2021-06-25 20:54:39 -0400

    ...

--- utils/gpu-usage.sh
+++ utils/gpu-usage.sh
@@ -10,6 +10,9 @@ echo -n " "
 echo -n $(( intOne * 100 / intTwo))
 echo -n "% "
 echo -n $foo
-# echo -n " "
-# echo $(sensors | grep 'Video Fan' | sed 's/ //g' | sed 's/VideoFan\://')
-
+echo -n " "
+echo -n $(sensors | grep 'fan1' | sed 's/ //g' | sed 's/fan1\://' | sed 's/\ *(.*//')
+echo -n " "
+echo -n $(sensors | grep 'fan2' | sed 's/ //g' | sed 's/fan2\://' | sed 's/\ *(.*//')
+echo -n " "
+echo -n $(sensors | grep 'fan3' | sed 's/ //g' | sed 's/fan3\://' | sed 's/\ *(.*//')
