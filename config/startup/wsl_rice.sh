e24f8ef30fdab0b9948c3b9770de460ea66e2a6e
Anthony Moore <68195709+antleemoore@users.noreply.github.com>
2022-07-15 12:50:16 -0400

    Create wsl_rice.sh

---
+++ config/startup/wsl_rice.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+# Command to run: wget -L https://raw.githubusercontent.com/antleemoore/Linux/arch/config/startup/wsl_rice.sh && sh ./wsl_rice.sh
+sudo pacman -Syyu git neovim python3 zsh neofetch lsd npm yarn cronie python-pip make gcc pacman-contrib ntfs-3g mlocate base-devel
+mkdir ~/repos ~/Projects ~/.config/coc ~/.config/nvim
+git clone https://github.com/antleemoore/Linux ~/repos/Linux
+git clone https://aur.archlinux.org/yay.git ~/repos/yay
+cd ~/repos/yay && makepkg -si  && cd ~
+git clone https://aur.archlinux.org/snapd.git ~/repos/snapd
+cd ~/repos/snapd && makepkg -si  && cd ~
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+ln -s ~/repos/Linux/scripts ~
+ln -s ~/repos/Linux/utils ~
+ln -s ~/repos/Linux/config/init.vim ~/.config/nvim
+ln -s ~/repos/Linux/config/plugin-config.vim ~/.config/nvim
+ln -s ~/repos/Linux/config/.vimrc ~
+ln -s ~/repos/Linux/config/.vimset ~
+ln -s ~/repos/Linux/config/.vimremap ~
+python -m pip install --user --upgrade pynvim
+nvim +PluginInstall +qall
+cd ~/.vim/bundle/coc.nvim && npm install && cd -
+ln -s ~/repos/Linux/config/ultisnip ~/.config/coc
+mkdir -p ~/.vim/pack/mbbill/start
+cd ~/.vim/pack/mbbill/start
+git clone https://github.com/mbbill/undotree.git
+vim -u NONE -c "helptags undotree/doc" -c q
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+rm ~/.zshrc
+ln -s ~/repos/Linux/config/.zshrc ~
+ln -s ~/repos/Linux/config/.zshaliases ~
+ln -s ~/repos/Linux/config/.zshprompt ~
+ln -s ~/repos/Linux/config/.sfdxalias ~
+yay -S pfetch
+sudo ln -s /var/lib/snapd/snap /snap
